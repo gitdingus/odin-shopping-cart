7eb3a0ea64a391c5d4313c2c74d663a572296c1e
Josh S <aaabccc87@protonmail.com>
2022-10-31 13:51:30 -0400

    Import images into project

---
+++ src/product-images.js
@@ -0,0 +1,29 @@
+import bevelGauge from './product-images/bevel-gauge.jpg';
+import chisel from './product-images/chisel.jpg';
+import combinationSquare from './product-images/combination-square.jpg';
+import handPlane from './product-images/hand-plane.jpg';
+import pencil from './product-images/pencil.jpeg';
+import saw from './product-images/saw.jpg';
+import tapeMeasure from './product-images/tape-measure.jpeg';
+import woodGlue from './product-images/wood-glue.jpg';
+import bench from './product-images/woodworking-bench.jpg';
+import clamp from './product-images/woodworking-clamp.png';
+import mallet from './product-images/woodworking-mallet.jpg';
+import vise from './product-images/woodworking-vise.jpg';
+
+const images = {
+  bevelGauge,
+  chisel,
+  combinationSquare,
+  handPlane,
+  pencil,
+  saw,
+  tapeMeasure,
+  woodGlue,
+  bench,
+  clamp,
+  mallet,
+  vise,
+};
+
+export default images;
