8a2aef5bbb665e5971ab7837247b932c114cd0e2
Josh S <aaabccc87@protonmail.com>
2022-10-31 13:52:42 -0400

    Export an array of product objects

---
+++ src/product-list.js
@@ -0,0 +1,78 @@
+import images from './product-images.js';
+
+const productList = [
+  {
+    uuid: '1',
+    title: 'Bevel Gauge',
+    imagePath: images.bevelGauge,
+    price: 10.99,
+  },
+  {
+    uuid: '2',
+    title: 'Chisel',
+    imagePath: images.chisel,
+    price: 8.99,
+  },
+  {
+    uuid: '3',
+    title: 'Combination Square',
+    imagePath: images.combinationSquare,
+    price: 13.99
+  },
+  {
+    uuid: '4',
+    title: 'Hand Plane',
+    imagePath: images.handPlane,
+    price: 25.99,
+  },
+  {
+    uuid: '5',
+    title: 'Pencil',
+    imagePath: images.pencil,
+    price: .99,
+  },
+  {
+    uuid: '6',
+    title: 'Hand Saw',
+    imagePath: images.saw,
+    price: 13.99,
+  },
+  {
+    uuid: '7',
+    title: 'Tape Measure',
+    imagePath: images.tapeMeasure,
+    price: 11.99,
+  },
+  {
+    uuid: '8',
+    title: 'Wood Glue',
+    imagePath: images.woodGlue,
+    price: 4.99,
+  },
+  {
+    uuid: '9',
+    title: 'Woodworking Bench',
+    imagePath: images.bench,
+    price: 499.99,
+  },
+  {
+    uuid: '10',
+    title: 'Clamp',
+    imagePath: images.clamp,
+    price: 14.99,
+  },
+  {
+    uuid: '11',
+    title: 'Mallet',
+    imagePath: images.mallet,
+    price: 19.99,
+  },
+  {
+    uuid: '12',
+    title: 'Woodworking Vise',
+    imagePath: images.vise,
+    price: 59.99,
+  },
+];
+
+export default productList;
