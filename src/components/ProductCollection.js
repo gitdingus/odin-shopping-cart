4e242af5370f88218183de86b66e8afd8ee0438f
Josh S <aaabccc87@protonmail.com>
2022-11-01 23:06:53 -0400

    Ability to sort products, styling.

--- src/components/ProductCollection.js
+++ src/components/ProductCollection.js
@@ -1,29 +1,107 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ProductThumb from './ProductThumb';
 import styles from './styles/product-collection.module.css';
 
-function ProductCollection ({
-  productList,
-  requestAddToCart,
-  productClick,
-}) {
-  return (
-    <div className={styles.productCollection}>
-      {
-        productList.map((product) => {
-          return (
-            <ProductThumb
-              key={product.uuid}
-              product={product}
-              requestAddToCart={requestAddToCart}
-            />
-          )
-        })
-      }
-    </div>
-  )
+const alphabetAscendingComparator = (item1, item2) => {
+  const a = item1.title;
+  const b = item2.title;
+  if (a > b) return 1;
+  if (a < b) return -1;
+  if (a === b) return 0;
+};
+const alphabetDescendingComparator = (item1, item2) => {
+  const a = item1.title;
+  const b = item2.title;
+  if (a < b) return 1;
+  if (a > b) return -1;
+  if (a === b) return 0;
+};
+const priceAscendingComparator = (item1, item2) => {
+  const a = item1.price;
+  const b = item2.price;
+  if (a > b) return 1;
+  if (a < b) return -1;
+  if (a === b) return 0;
+};
+const priceDescendingComparator = (item1, item2) => {
+  const a = item1.price;
+  const b = item2.price;
+  if (a < b) return 1;
+  if (a > b) return -1;
+  if (a === b) return 0;
+};
+
+const comparatorFunction = (mode) => {
+  let sortFn;
+
+  switch (mode) {
+    case "alphabet-ascending":
+      sortFn = alphabetAscendingComparator;
+      break;
+    case "alphabet-descending":
+      sortFn = alphabetDescendingComparator;
+      break;
+    case "price-ascending":
+      sortFn = priceAscendingComparator;
+      break;
+    case "price-descending":
+      sortFn = priceDescendingComparator;
+      break;
+    default:
+      sortFn = alphabetAscendingComparator;
+      break;
+  }
+
+  return sortFn;
 }
+
+    function ProductCollection ({
+      productList,
+      requestAddToCart,
+      productClick,
+    }) {
+      const [sortOrder, setSortOrder] = useState("");
+      const [products, setProducts] = useState(productList);
+
+      const sortProducts = (e) => {
+        setSortOrder(e.target.value);
+      }
+
+      useEffect(() => {
+        const newProducts = products.slice();
+
+        newProducts.sort(comparatorFunction(sortOrder));
+        setProducts(newProducts);
+      }, [sortOrder, products]);
+      
+      return (
+        <div>
+          <div className={styles.options}>
+            <label htmlFor="sort-order">Sort by:</label>
+            <select id="sort-order" value={sortOrder} onChange={sortProducts}>
+              <option value="alphabet-ascending">Alphabetical (A-z)</option>
+              <option value="alphabet-descending">Alphabetical (Z-a)</option>
+              <option value="price-ascending">Price (low-high)</option>
+              <option value="price-descending">Price (high-low)</option>
+            </select>
+          </div>
+          <div className={styles.productCollection}>
+            {
+              products.map((product) => {
+                return (
+                  <ProductThumb
+                    key={product.uuid}
+                    product={product}
+                    requestAddToCart={requestAddToCart}
+                  />
+                )
+              })
+            }
+          </div>
+        </div>
+      )
+    }
 
 ProductCollection.propTypes = {
   productList: PropTypes.arrayOf(PropTypes.shape({
